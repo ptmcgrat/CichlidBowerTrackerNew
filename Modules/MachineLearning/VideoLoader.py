efb8dedcadc91fd92288f96c50233d13dff57754
Patrick McGrath <ptmcgrat@gmail.com>
2020-01-27 16:20:22 -0500

    First

--- Modules/MachineLearning/VideoLoader.py
+++ Modules/MachineLearning/VideoLoader.py
@@ -61,7 +61,7 @@ class VideoLoader(data.Dataset):
 
 		# Normalize each channel data
 		for c in range(3):
-			print(video.shape
+			print(video.shape)
 			cropped_video[c] = (cropped_video[c] - self.means[index][c])/self.stds[index][c]
 
 		# Return tensor, label, and filename
