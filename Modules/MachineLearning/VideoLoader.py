42658dcce5944a3acaaed9dc70c28eb30ebd999f
Patrick McGrath <ptmcgrat@gmail.com>
2020-01-27 15:19:49 -0500

    First

--- Modules/MachineLearning/VideoLoader.py
+++ Modules/MachineLearning/VideoLoader.py
@@ -1,5 +1,6 @@
 import os
 from torchvision import transforms
+import torch.utils.data as data
 from skvideo import io as vp
 
 #torchvision.models.video.r3d_18(pretrained=False, progress=True, **kwargs)
