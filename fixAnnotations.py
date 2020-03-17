c2d097edd3226dcc0796e94d10e4944887ac1926
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2020-03-17 11:38:14 -0400

    test

--- fixAnnotations.py
+++ fixAnnotations.py
@@ -7,6 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('User1', type = str, help = 'Which user annotations to compare')
 parser.add_argument('User2', type = str, help = 'Which user annotations to compare')
 parser.add_argument('ProjectID', type = str, help = 'Project to analyze')
+parser.add_argument('-p', '--Practice', action = 'store_true', help = 'Use if you dont want to save annotations')
+
 
 args = parser.parse_args()
 
@@ -29,4 +31,9 @@ new_dt = pd.read_csv(temp_dt)
 old_dt = old_dt.append(new_dt, sort = 'False').drop_duplicates(subset = ['ProjectID', 'Framefile', 'User', 'Sex', 'Box'], keep = 'last').sort_values(by = ['ProjectID', 'Framefile'])
 old_dt.to_csv(fm_obj.localBoxedFishFile, sep = ',', columns = ['ProjectID', 'Framefile', 'Nfish', 'Sex', 'Box', 'CorrectAnnotation','User', 'DateTime'])
 
-fm_obj.uploadData(fm_obj.localBoxedFishFile)
+if not args.Practice:
+	fm_obj.uploadData(fm_obj.localBoxedFishFile)
+
+subprocess.run(['rm', '-rf', self.localProjectDir])
+subprocess.run(['rm', '-rf', self.localAnnotationDir])
+
